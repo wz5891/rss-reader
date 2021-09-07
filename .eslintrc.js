f5923d896ffaea8d8de73bf41d61a735ee2cf61b
liwei <liwei@iautorun.com>
2021-09-07 17:00:47 +0800

    elint报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: '',
+  "parserOptions": {
+    "ecmaVersion": 7,
+    "sourceType": "module",
+    "ecmaFeatures": { // 添加ES特性支持，使之能够识别ES6语法
+      "jsx": true
+    }
+  },
+  "rules": {
+    "semi": "warn"
+  }
 };
