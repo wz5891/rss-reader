640896266fe53ebc9ed3ade1b38b616d4c43358d
liwei <liwei@iautorun.com>
2021-09-08 09:44:49 +0800

    添加rss parse

--- src/views/rssListScreen.js
+++ src/views/rssListScreen.js
@@ -1,9 +1,21 @@
 import { Layout, Text } from '@ui-kitten/components';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
+import * as rssParser from 'react-native-rss-parser';
 
-function RssListScreen() {
+const RssListScreen = (props) => {
+    useEffect(() => {
+        fetch('https://www.ruanyifeng.com/blog/atom.xml')
+            .then((response) => response.text())
+            .then((responseData) => rssParser.parse(responseData))
+            .then((rss) => {
+                console.log(rss.title);
+                console.log(rss.items.length);
+                console.log(JSON.stringify(rss));
+            });
+    }, [])
+
     return (
         <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text category='h1'>RSSLIST</Text>
