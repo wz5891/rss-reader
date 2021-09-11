5cd89afadc5fd07c55894ff6aa8ed1a70444c051
liwei <liwei@iautorun.com>
2021-09-11 08:57:55 +0800

    redux debug工具

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,10 +2,11 @@ import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from '../reducers';
 
 const middleware = __DEV__ ?
-    applyMiddleware(promise, thunk, createLogger())
+    composeWithDevTools(applyMiddleware(promise, thunk, createLogger()))
     :
     applyMiddleware(promise, thunk);
 
