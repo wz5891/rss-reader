00085626b858300cd022513d04c026e7b44b878b
liwei <liwei@iautorun.com>
2021-09-14 11:13:24 +0800

    修改

--- src/views/settingScreen.js
+++ src/views/settingScreen.js
@@ -13,11 +13,27 @@ function SettingScreen(props) {
 
     return (
         <Layout style={styles.content}>
-            <Layout style={styles.item}>
-                <Text category="p1">夜间模式</Text>
+            <Layout style={styles.section}>
+                <Layout style={styles.item}>
+                    <Text category="p1">夜间模式</Text>
 
-                <Toggle checked={props.setting.get('isNight')} onChange={onIsNightChange}>
-                </Toggle>
+                    <Toggle checked={props.setting.get('isNight')} onChange={onIsNightChange}>
+                    </Toggle>
+                </Layout>
+            </Layout>
+
+            <Text category="s2">关于</Text>
+            <Layout style={styles.section}>
+                <Layout style={styles.item}>
+                    <Text category="p1">作者</Text>
+
+                    <Text category="p1">快刀</Text>
+                </Layout>
+                <Layout style={styles.item}>
+                    <Text category="p1">版本号</Text>
+
+                    <Text category="p1">1.0</Text>
+                </Layout>
             </Layout>
         </Layout>
     )
@@ -28,11 +44,21 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10
     },
+    section: {
+        borderWidth: 0.5,
+        padding: 10,
+        borderRadius: 4,
+        borderColor: '#93a1b2',
+        marginBottom: 20,
+        marginTop: 3
+    },
     item: {
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
+        paddingTop: 7,
+        paddingBottom: 7
     }
 })
 
